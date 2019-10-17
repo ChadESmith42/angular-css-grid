fee8608cfacfd2eb4374e75d00b8a1371f081b07
Chad Smith <Chad.Smith@BlueKC.com>
2019-10-17 14:26:09 -0500

    initial-commit.

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+
+export class NavigationComponent implements OnInit {
+  public screenHeight: number;
+  public screenWidth: number;
+
+  constructor () {
+   }
+
+  ngOnInit() {
+  }
+}
